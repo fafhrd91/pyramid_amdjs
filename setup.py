0ab8f36884da93aa659cc2900ffbe55b8939bf6a
Nikolay Kim <fafhrd91@gmail.com>
2012-11-08 11:31:42 -0800

    fix dep

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 version='0.1'
 
 install_requires = ['setuptools',
-                    'pyramid >= 1.4.0a2',
+                    'pyramid >= 1.4a2',
 ]
 
 if sys.version_info[:2] == (2, 6):
